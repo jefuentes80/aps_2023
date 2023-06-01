55a25aac6bfd3344511e61e4d0d37097971f0b21
Pablox <palchavez@scl.edu.gt>
2023-06-01 10:50:25 -0600

    Código ya hecho

---
+++ js/components/footer_contact/footer.js
@@ -0,0 +1,20 @@
+/*Sección del footer*/
+
+
+
+
+function llamar_footer() {
+    
+    let pie_pagina = document.querySelector("#footer");
+    pie_pagina.innerHTML = `
+    
+        <div class="pie_de_pagina">
+            <div class="Titulo">
+                <h1>Autors: </h1>
+            </div>
+            <div class="autores">
+                
+            </div>
+        </div>
+    `
+}
