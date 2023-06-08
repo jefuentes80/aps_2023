3aeec0e1a9f2fb61f3b96a9448c4e78233a26857
Claudia <clperez@scl.edu.gt>
2023-06-08 08:15:05 -0600

    Estructura base

--- js/components/refaccion/refaccion.js
+++ js/components/refaccion/refaccion.js
@@ -1,2 +1,57 @@
 // Componentes principal
 //Claudia Marisol Pérez Cruz
+
+async function actualizarGato2(apii){
+    let hola2 = await fetch(apii); 
+    let datoo = await hola2.json();
+
+    let ddd = document.querySelector(".desayuno")
+    
+ 
+        let div = document.createElement("div")
+
+        div.innerHTML= `
+        <div class="refaccion">
+                <div class="item"></div>    
+                <div class="item"></div>
+                <div class="item"></div>
+
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+        </div>
+
+        `
+        ddd.appendChild(div);
+
+ }
+
+actualizarGato2(apiiUrl);
