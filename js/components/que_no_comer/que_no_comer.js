9e8bf7b99da67333efcf9272ca0e961a3ddcd6a0
santizo-z <santizozamoradavid@gmail.com>
2023-06-08 11:30:31 -0600

    actualizanción 08

--- js/components/que_no_comer/que_no_comer.js
+++ js/components/que_no_comer/que_no_comer.js
@@ -5,7 +5,8 @@ function no_comer(){
 
     let item = `
         <div> <img src="${evitar_no_comer[0].img} " alt=""> </div>
+        <h1> ${evitar_no_comer[0].titulo} </h1>
+        <p> ${evitar_no_comer[0].dsescripcion} </p>
     `;
-
-    return "hola mundo";
+    let comer_al_dom = document.querySelector(".no-consumir")
 }
