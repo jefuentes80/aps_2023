fdaea867acf6302e9bd0ea8ffff3752a789a763d
Melissa <mnjuarez@scl.edu.gt>
2023-06-08 08:00:48 -0600

    Iniciando base de datos

--- js/components/desayunos/data_sesayunos.js
+++ js/components/desayunos/data_sesayunos.js
@@ -1,12 +1,48 @@
-// Objeto desayunos
+// Objeto desayunos 
+// Base de datos - Melissa J.
 let desayuno = [
     {
-        nombre: "huevos",
-        descripcion: "------",
+        nombre: "Huevos Rancheros",
+        descripcion: "Los huevos rancheros son uno de los desayunos " +
+        "más típicos y tradicionales de México.",
         img: "url",
         ingredientes: {
-            igrediente1: "111",
-            igrediente2: "111",
+            ingrediente1: "Tortillas de maíz: dos piezas.",
+            ingrediente2: "2 cucharadas soperas de Frijoles refritos",
+            ingrediente3: "1/4 de Cebolla blanca",
+            ingrediente4: "2 a 3 Huevos",
+            ingrediente5: "Aceite vegetal para freír",
+            ingrediente6: "Sal y Pimienta negra",
+            ingrediente7: "2 cucharadas de perejil ",
+            ingrediente8: "2 cucharadas de queso rallado",
+        },
+        preparacion: {
+            paso1: "- Para empezar, hay que verter la salsa roja en una cacerola para calentarla.",
+            paso2: "- Tomar el par de tortillas y dorarlas en un sartén, usando una cantidad discreta"+
+                   "de aceite (solamente la necesaria para hacer dorar las tortillas por ambos lados,"+
+                   " sin dejarlas mucho tiempo porque se ponen duras).",
+            paso3: "- Una vez calientes las tortillas, untar sobre cada una una buena cucharada de "+
+                    "frijoles refritos. Cuidar que los frijoles queden bien esparcidos sobre toda la tortilla.",
+            paso4: "- Aprovechar el sartén y un poco más de aceite para freír los huevos, al gusto.",
+            paso5: "- Una vez listos los huevos, acomodar cada uno sobre una tortilla enfrijolada. "+
+                    "Para terminar el plato, bañar los huevos con la salsa roja caliente.",
+            paso6: "- Opcionalmente, decorar espolvoreando el perejil finamente picado y el queso."
+        }
+
+    },
+
+    //SECCIÓN DE AVENA ******************************************************************************************
+    {
+        nombre: "Avena con mantequilla de maní",
+        descripcion: "Avena durante la noche con mantequilla de maní hecha con 5 ingredientes en 5 minutos.",
+        img: "url",
+        ingredientes: {
+            ingrediente1: "1/2 taza de leche (Entera, descremada, vegetal a elección)",
+            ingrediente2: "3/4 Tbsp semillas de chia",
+            ingrediente3: "2 Tbsp mantequilla de maní salada natural o mantequilla de almendras",
+            ingrediente4: "1 Tbsp de endulzante al gusto (jarabe de arce, azúcar de coco, azúcar morena o stevia)",
+            ingrediente5: "1/2 taza avena",
+            ingrediente6: "OPCIONAL: Topings como frutsas, granola y semillas"
         },
         preparacion: {
             pasa1: "- ososos",
@@ -15,5 +51,36 @@ let desayuno = [
             pasa4: "- ososos",
         }
 
+    },
+
+]
+
+
+let mis_comidas = [
+    {
+    nombre: "Barras de Cereal",
+    descripcion: "Aprende a preparar unas ricas Barras de "+
+    "Cereal Sin Gluten, Saludables y muy fáciles con 10 porciones.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "2 Tazas de pipocas de quínoa o arroz inflado",
+        ingrediente2: "1 Taza de manjar",
+        ingrediente3: "1 huevo",
+        ingrediente4: "1/2 Taza de almendras, semillas de girasol o avena tostada",
+        ingrediente5: "1 Taza de coco rallado",
+        ingrediente6: "1 taza de semillas o frutos secos surtidos ",
+    },
+    preparacion: {
+        paso1: "- En un bowl junta todos los ingredientes y mezcla hasta lograr unir completamente"+
+                ", formando una masa húmeda. Reserva.",
+        paso2: "- Sobre una lata de horno, con papel mantequilla espolvorea la mitad del coco rallado"+
+               " y dispone la masa esparciéndola formando un rectángulo, cubriendo con el resto del coco,"+
+               "lleva a horno pre-calentado durante 10 minutos a 200 °C. Hornea a 180°C durante 18 a 20 "+
+               "minutos, o bien hasta dorar levemente.",
+        paso3: "- Retira del horno y deja enfriar. Corta rectángulos de 10 cm x 3 cm. Guárdalo envueltos"+
+               "en papel metálico o plástico por unidades o bien en un frasco de vidrio con tapa. Recuerda"+
+               " que la porción sugerida es de una porción por persona."
+    }
+            
     }
 ]
