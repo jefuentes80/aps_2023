5bc02e277fb953b12b971214b28880ad986b61b6
Axel <adalvarez@scl.edu.gt>
2023-06-09 07:34:39 -0600

    Base1

--- js/components/cena/data_cena.js
+++ js/components/cena/data_cena.js
@@ -2,19 +2,50 @@
 /*Axel Daniel Alvarez López*/
 let desayuno = [
     {
-        nombre: "",
-        descripcion: "------",
+        nombre: "ENSALADA DE ARROZ",
+        descripcion: "La comida rápida también puede ser saludable. Mira esta deliciosa ensalada combinada con arroz. Tiempo de elaboración: 30 minutos",
         img: "url",
         ingredientes: {
-            igrediente1: "111",
-            igrediente2: "111",
+            igrediente1: "Arroz al gusto",
+            igrediente2: "Caldo de verduras",
+            igrediente2: "Espinacas",
+            igrediente2: "Aceitunas",
+            igrediente2: "Tomates",
+            igrediente2: "Achoas",
+            igrediente2: "Ajo",
+            igrediente2: "1 cebolla",
+            igrediente2: "Sal",
+            igrediente2: "Aceite",
+            igrediente2: "Vinagre balsámico",
         },
         preparacion: {
-            pasa1: "- ososos",
-            pasa2: "- ososos",
-            pasa3: "- ososos",
-            pasa4: "- ososos",
+            pasa1: "- Pica el ajo y la cebolla muy finos. En una sartén con un par de cucharadas de aceite, sofríelo todo hasta que la cebolla esté transparente. Tendrás que hacerlo a fuego lento. ",
+            pasa2: "- Añade el arroz, riega con el caldo de verduras y deja cocer durante 20 minutos.",
+            pasa3: "-Escurre y sirve.",
+            pasa4: "- Lava las verduras y trocéalas al gusto.",
+            pasa5: "- Sirve la ensalada fría, aliñada con aceite, sal y vinagre balsámico."
         }
-
+    },
+    {
+            nombre: "Pizza expres de pan de molde",
+            descripcion: "Tiempo: 25 minutos /Fácil/Económico",
+            img: "url",
+            ingredientes: {
+                igrediente1: "8 rebanadas de pan de molde sin corteza",
+                igrediente2: "250g de tomate frito casero",
+                igrediente2: "150g de beicon, jamon o tocino",
+                igrediente2: "150g de queso rallado",
+                igrediente2: "8 rodajas de mozzarella",
+                igrediente2: "1/2 calabacín",
+                igrediente2: "Unas hojas de albahaca",
+                igrediente2: "Orégano",
+                igrediente2: "Aceíte de oliva extra virgen",
+            },
+            preparacion: {
+                pasa1: "- Pica el beicon. Lava el calabacín y córtalo en láminas finas con una mandolina. ",
+                pasa2: "- Forra la placa del horno con papel sulfurizado y reparte encima las rebanadas. Úntalas con el tomate y espolvoréalas con orégano. Reparte encima el queso rallado, el beicon, el calabacín y la mozzarella.",
+                pasa3: "- Hornéalas 7 minutos en el horno precalentado a 180° C, hasta que el queso se funda. Sirve con la albahaca y un hilo de aceite.",
+            }
+    
     }
 ]
