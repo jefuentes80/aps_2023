09aac29cff3173c4f8bf4c1a0df6afac97adb598
Melissa <mnjuarez@scl.edu.gt>
2023-06-09 07:32:16 -0600

    prueba corta

--- js/components/desayunos/desayunos.js
+++ js/components/desayunos/desayunos.js
@@ -1,10 +1,11 @@
 // Componentes principal
 
 function prueba(){
-let titulo = document.querySelector(".desayuno");
+let titulo = document.querySelector("#desayuno");
 
 let div = document.createElement("div")
-div.innerHTML= `<h1>DESAYUNOS!!</h1>`;
+div.innerHTML= `
+    <h1>Desayunos Keily & Melissa !!</h1> `;
 div.classList.add("titulo")
 titulo.appendChild(div);
 
