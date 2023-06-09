aa47d04c2335b9ebf90eb308fdf887baadd001bf
Pablox <palchavez@scl.edu.gt>
2023-06-09 07:41:47 -0600

    Base del footer terminada

--- js/components/footer_contact/footer.js
+++ js/components/footer_contact/footer.js
@@ -5,14 +5,22 @@ function llamar_footer() {
     
     let pie_pagina = document.querySelector("#footer");
     pie_pagina.innerHTML = `
-    
         <div class="pie_de_pagina">
             <div class="Titulo">
-                <h1>Autors: </h1>
+                <h1>Autores: </h1>
             </div>
             <div class="autores">
-                <h2> Esto es un trabajo de los alumnos de 5° Bachillerato en Computación</h2>
-                <p>
+                <ul>
+                    <li><p>Byron Geovanni Guerra Hérnandez</p></li>
+                    <li><p>Keily Belén Telles Pérez</p></li>
+                    <li><p>Pablo Alberto Chávez López</p></li>
+                    <li><p>Melissa Noemi Juárez Colindres</p></li>
+                    <li><p>Juan Pablo Talé López</p></li>
+                    <li><p>Claudia Marisol Pérez Cruz</p></li>
+                    <li><p>Axel Daniel Alvarez ópez</p></li>
+                    <li><p>Alejandra Mercedes Zitá Juárez</p></li>
+                    <li><p>David Enrique Santizo Zamora</p></li>
+                </ul>
             </div>
         </div>
     `;
