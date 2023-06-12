f3c5e112b791e30e6924ad1e7e1efcd746d6fa3b
Axel <adalvarez@scl.edu.gt>
2023-06-12 12:54:25 -0600

    Final diseño

--- js/components/cena/cena.js
+++ js/components/cena/cena.js
@@ -1,27 +1,134 @@
 /// Crea los elementos HTML utilizando el DOM
 function primera_parte(){
   let elementos =
-  `  <section class="gallery" id="Galería">
-        <div class="contenedor">
-          <h2 class="subtitulo">Cenas</h2>
-          <div class="contenedor-galeria">
-            <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" class="img-galeria">
-            <img src="https://i.pinimg.com/564x/cc/af/74/ccaf74fd55e11d91fe6c50fb8d0b6f38.jpg" alt="" class="img-galeria">
-            <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" class="img-galeria">
-            <img src="https://i.pinimg.com/564x/cc/af/74/ccaf74fd55e11d91fe6c50fb8d0b6f38.jpg" alt="" class="img-galeria">
-            <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" class="img-galeria">
-            <img src="https://i.pinimg.com/564x/cc/af/74/ccaf74fd55e11d91fe6c50fb8d0b6f38.jpg" alt="" class="img-galeria">
-            <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" class="img-galeria">
-            <img src="https://i.pinimg.com/564x/cc/af/74/ccaf74fd55e11d91fe6c50fb8d0b6f38.jpg" alt="" class="img-galeria">
-            <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" class="img-galeria">
-          </div>
-        </div>
-      </section>
-      
-      <section class="imagen-light">
-        <i class="fas fa-times close"></i>
-        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-icons.com%2Fes%2Ficono%2Fx-c%25C3%25ADrculo%2F174835&psig=AOvVaw3CtJFE4uvYrp6lfVeJegqD&ust=1686667536133000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICR-av8vf8CFQAAAAAdAAAAABAE" alt="" class="agregar-imagen">
-      </section>
+  `  <h1>Galeria de Cenas</h1>
+
+  <div class="main-gallery">
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" />
+      </div>
+      <p>Cena 1</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/fc/ec/ee/fceceee44174a2bd96d21b32d676472a.jpg" alt="" />
+      </div>
+      <p>Cena 2</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" />
+      </div>
+      <p>Cena 3</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/fc/ec/ee/fceceee44174a2bd96d21b32d676472a.jpg" alt="" />
+      </div>
+      <p>Cena 4 </p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" />
+      </div>
+      <p>Cena 5</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/fc/ec/ee/fceceee44174a2bd96d21b32d676472a.jpg" alt="" />
+      </div>
+      <p>cena 6</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" />
+      </div>
+      <p>Cena 7</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/fc/ec/ee/fceceee44174a2bd96d21b32d676472a.jpg" alt="" />
+      </div>
+      <p>Cena 8</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" />
+      </div>
+      <p>Cena 9</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/fc/ec/ee/fceceee44174a2bd96d21b32d676472a.jpg" alt="" />
+      </div>
+      <p>Cena 10</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" />
+      </div>
+      <p>Cena 11</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/57/37/4c/57374cb38802d35fe9e9825a2502ab33.jpg" alt="" />
+      </div>
+      <p>Cena 12</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/fc/ec/ee/fceceee44174a2bd96d21b32d676472a.jpg" alt="" />
+      </div>
+      <p>Cena 13</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/57/37/4c/57374cb38802d35fe9e9825a2502ab33.jpg" alt="" />
+      </div>
+      <p>Cena 14</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" />
+      </div>
+      <p>Cena 15</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/fc/ec/ee/fceceee44174a2bd96d21b32d676472a.jpg" alt="" />
+      </div>
+      <p>Cena 16</p>
+    </article>
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/54/5e/d7/545ed7a75d832ea4a7faaab086ee9684.jpg" alt="" />
+      </div>
+      <p>Cena 15</p>
+    </article>
+    
+    <article>
+      <div class="photo">
+        <img src="https://i.pinimg.com/564x/fc/ec/ee/fceceee44174a2bd96d21b32d676472a.jpg" alt="" />
+      </div>
+      <p>Cena 16</p>
+    </article>
+  </div>
     `;
 
     let caja = document.getElementById("cena")
@@ -33,26 +140,4 @@ caja.innerHTML = elementos;
 
 primera_parte();
 
-const imagenes = document.querySelectorAll('.img-galeria');
-const imagenesLight = document.querySelector('.agregar-imagen');
-const contenedorLight = document.querySelector('.imagen-light');
 
-imagenes.forEach(imagen =>{
-    imagen.addEventListener('click', ()=>{
-        aparecerImagen(imagen.getAttribute('src'))
-    
-    })
-})
-
-contenedorLight.addEventListener('click', (e) =>{
-    if(e.target !== imagenesLight){
-        contenedorLight.classList.remove('show');
-        imagenesLight.classList.remove('showImage'); 
-    }
-})
-
-const aparecerImagen = (imagen) =>{
-    imagenesLight.src = imagen;
-    contenedorLight.classList.add('show');
-    imagenesLight.classList.add('showImage');
-}
