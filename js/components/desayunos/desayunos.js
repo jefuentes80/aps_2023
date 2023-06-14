94e9db99efea4edc5d45bb95e784b7f01dc26e0f
Keily <kbtelles@scl.edu.gt>
2023-06-14 13:28:55 -0600

    integración de jsc module

--- js/components/desayunos/desayunos.js
+++ js/components/desayunos/desayunos.js
@@ -1,13 +1,17 @@
 // Componentes principal
+import { ola } from "./sabias_que.js";
+
 
 function prueba(){
-let titulo = document.querySelector(".desayuno");
+let titulo = document.querySelector("#desayuno");
 
 let div = document.createElement("div")
 div.innerHTML= `<h1>DESAYUNOS!!</h1>`;
 div.classList.add("titulo")
 titulo.appendChild(div);
-
 }
 
 prueba();
+ola();
+
+
