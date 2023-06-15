cc69c1e4d8e10618c0e32f1e7be46c1e8327c4cc
Keily <kbtelles@scl.edu.gt>
2023-06-15 11:47:16 -0600

    Agregación de desayunos

--- js/components/desayunos/data_sesayunos.js
+++ js/components/desayunos/data_sesayunos.js
@@ -4,7 +4,7 @@
 let desayuno = [
     //SECCION DE HUEVOS ****************************************************************************************
     {
-        nombre: "Huevos Rancheros",
+        nombre: "HUEVOS RANCHEROS",
         descripcion: "Los huevos rancheros son uno de los desayunos " +
         "más típicos y tradicionales de México.",
         img: "url",
@@ -33,7 +33,7 @@ let desayuno = [
 
     },
     {
-        nombre: "Huevos con salchicha",
+        nombre: "HUEVOS CON SALCHICHA",
         descripcion: "Una receta fácil de preparar con ingredientes accesibles",
         img: "url",
         ingredientes: {
@@ -55,7 +55,7 @@ let desayuno = [
 
     },
     {
-        nombre: "Burrito de huevo con tocino",
+        nombre: "BURRITO DE HUEVO CON TOCINO",
         descripcion: "Comienza tu día con un desayuno llenador, son fáciles de hacer y con un sabor que te encantará",
         img: "url",
         ingredientes: {
@@ -78,8 +78,68 @@ let desayuno = [
         }
 
     },
+    { /* K_telles*/
+    nombre: "HUEVOS REVUELTOS CREMOSOS CON YOGUR",
+    descripcion: "Los huevos son un gran alimento para empezar el día y con yogur elevan los beneficios de este alimento",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "2 huevos grandes",
+        ingrediente2: "2 cuch. de yogur griego",
+        ingrediente3: "1/2 cucharada de mantequilla",
+        ingrediente4: "2 cucharaditas de cebollino picado",
+        ingrediente5: "Sal y pimienta"
+    },
+    preparacion: {
+        paso1: "- En un bol mediano, bate los huevos y el yogur.",
+        paso2: "- En una sartén mediana a fuego medio-bajo, derrite la mantequilla. Cuando la mantequilla esté completamente derretida, vierte los huevos y cuando empiecen a cuajar, usa una espátula de silicona para mover suavemente el huevo en la sartén..",
+        paso3: "-  Cuando los huevos estén cuajados a tu gusto, incorpora el cebollino. Condimenta con sal y pimienta."
+    }
+    },
+
+
+    { /* K_telles*/
+    nombre: "TORTILLA DE JAMÓN Y QUESO",
+    descripcion: "Un fácil tortilla de jamon y queso ¡es el desayuno simple y satisfactorio perfecto para comenzar bien el día!",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "3 huevos grandes",
+        ingrediente2: "Sal",
+        ingrediente3: "1 cucharada de mantequilla",
+        ingrediente4: "1/2 taza de queso cheddar rallado",
+        ingrediente5: "1 cucharada de leche o agua",
+        ingrediente5: "1/2 taza de jamón picado"
+    },
+    preparacion: {
+        paso1: "-  En un tazón mediano, combine 3 huevos grandes, ⅛ de cucharadita de sal, 1 cucharada de leche o agua opcional (si se usa) y mezcle con un tenedor.",
+        paso2: "- En una sartén antiadherente de 9 pulgadas a fuego medio, derrita 1 cucharada de mantequilla.  Incline la sartén para que la mantequilla se extienda y cubra todo el fondo de la sartén.",
+        paso3: "-  Vierta los huevos batidos directamente en el centro de su sartén caliente. Luego, levante e incline la sartén para que la mezcla de huevo se extienda y cubra el fondo hasta el borde de la sartén.",
+        paso4: "-  Mientras los huevos comienzan a endurecerse, use su espátula para levantar los bordes para que el huevo crudo pueda llenarse debajo.",
+        paso5: "-  Una vez que se hayan llenado los espacios y se hayan redondeado los bordes, deje que su tortilla se cocine durante 10-15 segundos más. Rellena el omelet con la mitad de la ½ taza de queso cheddar y la ½ taza de jamón. Usando su espátula, doble un lado de la tortilla sobre los rellenos hacia el borde opuesto y ciérrelo.",
+        paso6: "- Deslice con cuidado la tortilla cocida fuera de la sartén y colóquela en un plato. Cúbralo con el queso cheddar y el jamón restantes y sirva de inmediato."
+    }
+    },
+
+
+    { /* K_telles*/
+    nombre: "CHORIZO Y HUEVOS (HUEVOS CON CHORIZO)",
+    descripcion: "Esta receta es muy sencillo ¡y solo requiere 4 ingredientes! Si no tiene huevos medianos, los grandes o extra grandes funcionarán bien.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "2 cucharaditas de aceite.",
+        ingrediente2: "9 onzas de chorizo ​​chorizo.",
+        ingrediente3: "6 huevos grandes.",
+        ingrediente4: "Sal"
+    },
+    preparacion: {
+        paso1: "-  Caliente 2 cucharaditas de aceite en una sartén grande a fuego medio. Una vez que el aceite esté caliente y comience a brillar, agregue 9 onzas de chorizo ​​a la sartén. Tomar un cuchara de madera y rompa la salchicha mientras se cocina. Continúe cocinando hasta que la carne esté bien cocida y ligeramente crujiente. (unos 7 minutos).",
+        paso2: "- Mientras se cocina el chorizo, rompa 6 huevos grandes en un tazón para mezclar. Añadir una pizca de sal si se desea (ten en cuenta que el chorizo ​​ya está bastante salado). revuelve los huevos con un batidor.",
+        paso3: "-  Vierta los huevos revueltos directamente en la sartén con el chorizo ​​y revuelva para combinar.",
+        paso4: "-  Deje que los huevos se cocinen hasta que estén configura a tu gusto luego servir de inmediato."
+       }
+    },
+
     {
-        nombre: "Tortillas de queso",
+        nombre: "TORTILLAS DE QUESO",
         descripcion: "La tortilla de queso salado es la comida reconfortante más fácil que podrías hacer",
         img: "url",
         ingredientes: {
@@ -104,7 +164,7 @@ let desayuno = [
     },
     //SECCIÓN DE AVENA ******************************************************************************************
     {
-        nombre: "Avena con mantequilla de maní",
+        nombre: "AVENA CON MANTEQUILLA DE MANÍ",
         descripcion: "Avena durante la noche con mantequilla de maní hecha con 5 ingredientes en 5 minutos.",
         img: "url",
         ingredientes: {
@@ -123,6 +183,92 @@ let desayuno = [
         }
 
     },
+    { /* K_telles*/
+    nombre: "HOT CAKES DE AVENA, MANZANA Y CANELA",
+    descripcion: "Empieza tus mañanas con el desayuno más saludable, fácil y delirante: hot cakes de avena, manzana y canela.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "1 taza de avena",
+        ingrediente2: "½ taza de leche",
+        ingrediente3: "1 huevo",
+        ingrediente4: "1 pizca de canela en polvol",
+        ingrediente5: "1 manzana",
+        ingrediente6: "Aceite"
+    },
+    preparacion: {
+        paso1: "- Licua la avena, la pizca de canela, la leche y el huevo.",
+        paso2: "- Ralla la manzana e incorpora a la mezcla de hot cakes, no es necesario licuar.",
+        paso3: "- Calienta un sartén con aceite.",
+        paso4: "- Verte un cucharon con la mezcla de hot cakes de avena y manzana.",
+        paso5: "- Cocina los hot cakes de avena y manzana durante 2 minutos o hasta que empiece a verse burbujas en la superficie, voltea y cocina del otro lado durante 2 minuto."
+    }
+    },
+
+    { /* K_telles*/
+    nombre: "TORTILLA DE AVENA Y VERDURA",
+    descripcion: "Para los amantes de los desayunos salados, hay muchas alternativas a las clásicas de tostadas con aceite y tomate. Por ejemplo esta tortilla de avena con verdura, un desayuno completo con un buena proporción de proteínas, hidratos de carbono de absorción lenta y verdura, que nos asegura la energía necesaria para toda la mañana.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "4 cucharadas de copos de avena",
+        ingrediente2: "3 huevos",
+        ingrediente3: "4 cucharadas de leche",
+        ingrediente4: "medio pimiento rojo",
+        ingrediente5: "1 puñado de espinacas",
+        ingrediente6: "2 cucharadas de aceite de oliva",
+        ingrediente7: "Sal"
+    },
+    preparacion: {
+        paso1: "- En un bol mezcla los copos de avena con la leche y deja reposar mientras preparas los otros ingredientes. Si tienes tiempo o tienes una digestión más lenta, puedes dejar los copos de avena en remojo toda la noche.",
+        paso2: "- Prepara la tortilla. Casca los huevos a un bol, bate y salpimienta.",
+        paso3: "- Prepara las verduras.  En una sartén, calienta una cucharada de aceite y sofríe el pimiento con un poco de sal, unos 5-7 minutos. Añade las espinacas y dañe un par de vueltas en la sartén. ",
+        paso4: "- Añade la verdura a la mezcla de los huevos. Incorpora los copos de avena remojados y mezcla bien.",
+        paso5: "- En la misma sartén en la que preparaste la verdura, calienta la otra cucharada de aceite de oliva, vierte la mezcla y cocina unos 2 minutos. Dale la vuelta a la tortilla, cocina por el otro lado y sirve."
+    }
+    },
+
+    { /* K_telles*/
+    nombre: "PAN DE AVENA",
+    descripcion: "La clave para que se trate de un desayuno realmente saludable es el pan, y lo ideal es elegir un buen pan 100% integral. Pero ¿y si probamos con esta receta de pan sin gluten?",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "275 g de harina de avena (certificada sin gluten si es preciso)",
+        ingrediente2: "350 ml de yogur natural",
+        ingrediente3: "1 cucharada de polvo para hornear",
+        ingrediente4: "1 cucharada de semillas de chía",
+        ingrediente5: "Sal"
+    },
+    preparacion: {
+        paso1: "- Precalienta el horno a 180 grados y unta un molde para pan con un poco de aceite o forra con papel vegetal.",
+        paso2: "- Si no tienes harina de avena, tritura los copos hasta obtenerla.",
+        paso3: "- En un bol mezcla todos los ingredientes secos. Añade el yogur hasta formar una masa. Deja reposar unos 10 minutos. ",
+        paso4: "- Vierte la masa en el molde, alisa, espolvorea con copos de avena o semillas, lleva al horno y hornea unos 40-45 minutos o hasta que el palillo introducido dentro sale seco. Deja enfriar"
+    }
+    },
+
+    {/* K_telles*/
+    nombre: "PIZZA DE AVENA",
+    descripcion: "Es delicioso realizar una pizza de avena, saludable y crujiente.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "100 g de avena",
+        ingrediente2: "100 ml de agua",
+        ingrediente3: "5 g de cebolla en polvo",
+        ingrediente4: "10 g de perejil o albahaca molida",
+        ingrediente5: "5 ml de aceite de oliva",
+        ingrediente6: "3 unidades de queso mozzarella en rodajas",
+        ingrediente7: "1 tomate pera en rodajas",
+        ingrediente8: "Orégano o perejil seco a gusto."
+
+    },
+    preparacion: {
+        paso1: "- En un cuenco, volcar la avena junto al agua y las especias. Integrar con una cuchara y dejar reposar por 5 a 10 minutos hasta que se forme un ¨engrudo¨ homogéneo.",
+        paso2: "- Incorporar el oliva e integrar. Mientras tanto, poner a calentar una sartén antiadherente a fuego medio bajo. ",
+        paso3: "- Extender la masa de avena en la sartén y dejar cocinar de 4 a 5 minutos, verás como los bordes se tornan más oscuros.En este punto, colocar encima el queso mozarella, las rodajas de tomate y el orégano o perejil (al gusto de cada uno).",
+        paso4: "- Tomar la tapa de la sartén y colocar por encima para permitir que el calor derrita el queso, de 3 a 4 minutos más.",
+        paso5: "- Al momento de servir, añadir un toque más de hierbas y aceite de oliva.."
+    }
+    },
+
     //SECCION DE YOGURT *****************************************************************************************************
     { /* K_telles*/
         nombre: "YOGUR DE FRESA Y VAINILLA",
@@ -173,14 +319,115 @@ let desayuno = [
         paso5: "- Limpia las fresas, lávalas y córtalas con el plátano restante en rodajas.",
         paso6: "- Reparte las tortitas en los platos intercalando tortitas y capas de fruta. Vierte la miel por encima y sirve."
     }
-},
+    },
+    { /* K_telles*/
+    nombre: "YOGUR CON QUINOA Y FRUTAS ROJAS",
+    descripcion: "Esta es una opción válida para quienes no quieren consumir copos o cereales clásicos de desayuno. Además, es refrescante y deliciosa.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "1 taza de yogur natural",
+        ingrediente2: "1 cucharadita de miel",
+        ingrediente3: "1/3 taza de quinoa cocida",
+        ingrediente4: "1 pocillo de frutas rojas congeladas."
+    },
+    preparacion: {
+        paso1: "- Coloca el yogur en la licuadora junto con las frutas rojas y el endulzante. Licúa hasta que todo se vuelva homogéneo..",
+        paso2: "- Coloca el contenido en una taza o compotera y añade la quinoa cocida."
+    }
+    },
+
+
+    { /* K_telles*/
+    nombre: "PANQUEQUES DE YOGUR Y AVENA",
+    descripcion: "Los panqueques son una preparación clásica, ideal para hacer desayunos saludables. Si bien hay diferentes recetas para elaborarlos, los que se hacen con yogur tienen una consistencia más densa que los tradicionales.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "1 taza de avena",
+        ingrediente2: "1/2 taza de yogur firme",
+        ingrediente3: "1 cucharada de pasta de maní",
+        ingrediente4: "1/2 cucharada de semillas de linaza",
+        ingrediente5: "1/2 cucharadita de aceite",
+        ingrediente6: "2 cucharadas de miel",
+        ingrediente7: "1 cucharada de pasta de maní y 1/2 manzana (para decorar)."
+
+    },
+    preparacion: {
+        paso1: "- En un bol, mezcla el yogur junto con la avena y las semillas de linaza.",
+        paso2: "- Luego, añade la pasta de maní y la cucharada de miel. Revuelve unos minutos hasta que todo se integre.",
+        paso3: "- Deja que este repose 5 minutos, mientras esparces el aceite en una panquequera.",
+        paso4: "- Cocina a fuego bajo, durante 2 minutos de cada lado..",
+        paso5: "- Esparce la pasta de maní por toda la superficie y corta la manzana en láminas finas. Colócalas por encima del panqueque y disfruta.",
+    }
+    },
+
+
+    { /* K_telles*/
+    nombre: "TOSTADAS DE YOGUR Y FRUTOS ROJOS",
+    descripcion: "Deliciosa receta de tostada francesa con yogur y frutos rojos.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "2 rebanadas gruesas de pan tipo brioche, de trigo o de masa madre",
+        ingrediente2: "1/4 taza de yogur",
+        ingrediente3: "1 huevo grande",
+        ingrediente4: "2 cuch. de sirope de arce, agave, miel o chocolate derretido",
+        ingrediente5: "Una pizca de canela",
+        ingrediente6: "Arándanos, fresas y/o frambuesas",
+        ingrediente7: "Azúcar glas, avena tostada, coco seco, virutas de chocolate (todo opcional), para servir."
+
+    },
+    preparacion: {
+        paso1: "- Precalentar un horno a 200 ºC. En un bol, bate el yogur, el huevo, el sirope de arce y la canela.",
+        paso2: "- Con los dedos o el dorso de una cuchara, presiona en el centro del pan para marcar y crear un hueco. Rellena con la mezcla de yogur. Cubre con las frutas. Coloca las rebanadas de pan preparadas en una bandeja del horno y hornea hasta que el pan esté dorado, de 10 a 13 minutos.",
+        paso3: "- Transfiere las tostadas a un plato y rocía con la cobertura que elijas y, opcionalmente, termina con una capa de azúcar en polvo."
+    }
+    },
+
+
+    { /* K_telles*/
+    nombre: "TORTITAS DE YOGUR",
+    descripcion: "Estos pancakes de yogur o tortitas son súper fáciles de preparar y necesitan pocos ingredientes que muchos tenemos en nuestras despensas.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "1 taza de harina",
+        ingrediente2: "1 y 2/3 taza de yogur",
+        ingrediente3: "2 huevos",
+        ingrediente4: "1 cuch. de levadura en polvo"
+    },
+    preparacion: {
+        paso1: "- Mezcla la harina y la levadura en un bol grande. En un recipiente aparte, bate los huevos y el yogur griego, luego incorpóralos a la mezcla de harina, formando una masa espesa.",
+        paso2: "- Vierte la masa en porciones en una sartén y cocina a fuego medio-bajo, usando una espátula o cuchara de silicona para darle forma de panqueque. Levanta el borde de la tortita para comprobar que está ligeramente dorada, desliza la espátula hasta el fondo y voltea el panqueque.",
+        paso3: "- Sirve las tortitas tibias con almíbar, caramelo o chocolate por encima."
+    }
+    },
+
+
+    { /* K_telles*/
+    nombre: "BANANA SPLITS DE YOGUT",
+    descripcion: "Una banana splits de yogur para desayunar es un delicioso alimento para empezar el día.",
+    img: "url",
+    ingredientes: {
+        ingrediente1: "1 plátano, cortado a lo largo",
+        ingrediente2: "1 taza de yogur",
+        ingrediente3: "Arándanos",
+        ingrediente4: "Fresas picadas",
+        ingrediente5: "4 cucharadas de Granola",
+        ingrediente6: "Miel, para rociar"
+
+    },
+    preparacion: {
+        paso1: "- Coloca el plátano en un plato alargado.",
+        paso2: "- Vierte el yogur encima.",
+        paso3: "-  Rocía con miel y decora con arándanos, fresas y granola."
+    }
+    }
+
 
 ]
 
 
 let mis_comidas = [
     {
-    nombre: "Barras de Cereal",
+    nombre: "BARRAS DE CEREAL",
     descripcion: "Aprende a preparar unas ricas Barras de "+
     "Cereal Sin Gluten, Saludables y muy fáciles con 10 porciones.",
     img: "url",
