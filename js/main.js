7b10efec548c987bdca9b530ba30bd1ede2c58dc
jefuentes <jefuentes@scl.edu.gt>
2023-06-16 11:16:40 -0600

    Funcionalidad del home

--- js/main.js
+++ js/main.js
@@ -1,24 +1,71 @@
 import { home_items } from './main_data.js';
 
+// SECCIÓNES DE LA APLICACIÓN
 let home = document.querySelector("#home");
+let desayuno = document.querySelector("#desayuno");
+let refaccion = document.querySelector("#refaccion");
+let cena = document.querySelector("#cena");
+let no_consumir = document.querySelector("#no-consumir");
 
-home_items.forEach((elemento) => {
-    let item = document.createElement("div");
+function iniciar_home() {
+    home_items.forEach((elemento) => {
+        let item = document.createElement("div");
 
-    item.classList.add("item-home");
-    item.classList.add(elemento.nombre);
-    item.innerHTML = `
-    <div class="box-img">
-        <img src="${elemento.img}">
-    </div>
-    <div class="contentenido">
-        <h2>Desayunos</h2>
-        <p>
-            Texto de muestra del contenido
-        </p>
-    </div>
-    `;
+        item.classList.add("item-home");
+        item.classList.add(elemento.nombre);
+        item.innerHTML = `
+        <div class="box-img">
+            <img src="${elemento.img}">
+        </div>
+        <div class="contentenido">
+            <h2>${elemento.nombre}</h2>
+            <p>
+                ${elemento.descripcion}
+            </p>
+        </div>
+        `;
+        home.appendChild(item);
 
-    home.appendChild(item);
+    });
+}
+iniciar_home();
 
-});
+// PROGRAMACIÓN DE ACTIVACIÓN
+function mostrar_seccion() {
+
+    // ÍTEMS DEL HOME
+    let Home = document.querySelector("#home");
+    let lista_secciones = document.querySelectorAll(".item-home");
+
+
+    function activar_seccion() {
+        Home.removeAttribute("id");
+        Home.classList.add("ocultar");
+
+        if (this.className == "item-home Desayuno") {
+            desayuno.classList.remove("ocultar");
+        } else if (this.className == "item-home Refaccion") {
+            refaccion.classList.remove("ocultar");
+        } else if (this.className == "item-home Cena") {
+            cena.classList.remove("ocultar");
+        } else if (this.className == "item-home No-consumir") {
+            no_consumir.classList.remove("ocultar");
+        }
+    }
+
+    lista_secciones.forEach((seccion) => {
+        seccion.addEventListener("click", activar_seccion);
+    });
+
+}
+
+function secciones_de_la_aplicacion() {
+    desayuno.classList.add("ocultar");
+    refaccion.classList.add("ocultar")
+    cena.classList.add("ocultar")
+    no_consumir.classList.add("ocultar")
+
+    mostrar_seccion();
+}
+
+secciones_de_la_aplicacion();
