9e72ae26eb9df2930e7d35eda302e080c210581e
santizo-z <santizozamoradavid@gmail.com>
2023-06-22 07:19:40 -0600

    base de datos

--- js/components/que_no_comer/data_no_comer.js
+++ js/components/que_no_comer/data_no_comer.js
@@ -1,13 +1,13 @@
- let evitar_no_comer = [
+export const evitar_no_comer = [
     {
         img : "https://cdn.www.gob.pe/uploads/document/file/285975/standard_vaso_de_gaseosa1.jpg",
         titulo : "Gaseosas",
-        dsescripcion : " las gaseosas poseen un alto contenido de azúcar y otros aditivos, como la cafeína, lo que desencadena en efecto diurético ocasionando que el cuerpo humano pierde más agua."
+        dsescripcion : "Las gaseosas poseen un alto contenido de azúcar y otros aditivos, como la cafeína, lo que desencadena en efecto diurético ocasionando que el cuerpo humano pierde más agua."
     },
     {
         img : "https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2018/07/secretos-de-la-comida-rapida-que-nadie-sabe.jpg",
         titulo : "Comida Rapida",
-        dsescripcion : "“provoca obesidad y todas las patologías consecuentes a ésta, como diabetes, hipertensión arterial, aterosclerosis, enfermedades cerebrovasculares, patologías renales, hígado graso, e incluso cáncer"
+        dsescripcion : "Provoca obesidad y todas las patologías consecuentes a ésta, como diabetes, hipertensión arterial, aterosclerosis, enfermedades cerebrovasculares, patologías renales, hígado graso, e incluso cáncer."
     },
     {
         img : "https://blog.auna.pe/hubfs/Imported_Blog_Media/comida-enlatada-2.jpg",
@@ -17,11 +17,33 @@
     {
         img : "https://www.topdoctors.mx/files/Image/large/142603aea415429be5bcb3fdc6b3dc52.png",
         titulo : "Alimentos fritos",
-        dsescripcion : "El exceso de frituras favorece el sobrepeso y las patologías cardiovasculares. Por su aporte de grasa, contribuyen al aumento del colesterol, triglicéridos y del porcentaje de grasa corporal"
+        dsescripcion : "El exceso de frituras favorece el sobrepeso y las patologías cardiovasculares. Por su aporte de grasa, contribuyen al aumento del colesterol, triglicéridos y del porcentaje de grasa corporal."
     },
     {
         img : "https://www.consumer.es/wp-content/uploads/2020/06/bebidas-isotonicas-ejemplos.jpg",
         titulo : "Bebidas deportivas y energéticas.",
         dsescripcion : "Muchas de estas bebidas tienen gran cantidad de azúcar y calorías. Las bebidas energéticas también tienen mucha cafeína. A menos que haga ejercicio suficientemente intenso como para sudar por una hora o más, es preferible tomar agua. También puede elegir las versiones bajas en calorías de estas bebidas."
+    },
+    {
+        img : "https://www.recetasnestle.com.pe/sites/default/files/2022-07/banner-como-hacer-macarrones-con%20queso.jpg",
+        titulo : "Comidas que contienen queso",
+        dsescripcion : "Las comidas que contengan queso con su contenido completo de grasa, como pizza, burritos y macarrones con queso.",
+    },
+    {
+        img : "https://aprende.guatemala.com/wp-content/uploads/2020/05/Recetas-de-bebidas-con-cafe%CC%81-guatemalteco-para-disfrutar-en-casa.jpg",
+        titulo : "Bebidas de cafe",
+        dsescripcion : "El café solo es bajo en calorías. Pero una vez que se añade leche alta en grasas, saborizantes azucarados y crema batida, aumenta el conteo de calorías.",
+    },
+    {
+        img : "https://recetasdecocina.elmundo.es/wp-content/uploads/2022/11/galletas-de-chocolate-receta-facil.jpg",
+        titulo : "Azúcares agregados",
+        dsescripcion : "El consumo excesivo de azúcares agregados, presentes en productos como refrescos, dulces, pasteles, galletas y cereales azucarados, puede llevar a la obesidad, la diabetes tipo 2, las enfermedades cardíacas y otros problemas de salud. Es recomendable limitar el consumo de estos alimentos y optar por fuentes de azúcar natural, como frutas frescas.",
+    },
+    {
+        img : "https://artemonte.com/wp-content/uploads/Beneficios-de-comer-embutido-768x432.png",
+        titulo : "Alimentos ricos en sal:",
+        dsescripcion : "El consumo excesivo de sal puede contribuir a la hipertensión arterial y aumentar el riesgo de enfermedades cardíacas. Se debe evitar el consumo excesivo de alimentos salados como los alimentos procesados, embutidos, enlatados, snacks salados y salsas comerciales.",
     }
+
+    
 ]
