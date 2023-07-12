e765b7a640751dc8ccfbbcd94ef6e3d652bca7f6
Claudia <clperez@scl.edu.gt>
2023-07-12 08:26:27 -0600

    Sabias que

--- js/components/refaccion/refaccion.js
+++ js/components/refaccion/refaccion.js
@@ -1,14 +1,10 @@
-// Componentes principal
-//Claudia Marisol Pérez Cruz
-
-
-import { refacciones } from "./data_refaccion.js";
+import { recetas } from "./prueba.js";
 
 function ola(){
 
-    let cajita = document.querySelector("#refaccion")
+    let cajita = document.querySelector(".recetas")
     
-    refacciones.forEach(element => {
+    recetas.forEach(element => {
 
         let gio = document.createElement("div")
         gio.classList.add("item")
@@ -25,62 +21,128 @@ function ola(){
 ola();
 
 
-/*function actu(){
 
-    let prueba = document.querySelector("#refaccion");*/
- 
+/*******************BUSCADOR */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { recetas } from "./prueba.js";
+
+function ola(){
+
+    let cajita = document.querySelector(".recetas")
     
- 
-     /*   let div = document.querySelector("#refaccion")
-        let caja = document.createElement("section");
+    recetas.forEach(element => {
 
+        let gio = document.createElement("div")
+        gio.classList.add("item")
+        gio.innerHTML= `
+        <img src= "${element["img"]}" class="imagenes_re">
+            <p class="nombres">${element["nombre"]} </p>
+            <p class="descripcion">${element["descripcion"]} </p>
 
-
-        caja.innerHTML= `
-        <div class="refaccion">
-                <div class="item">
-                <h1>refcciones juan Pablo  marisol</h1>
-                </div>    
-                <div class="item"></div>
-                <div class="item"></div>
-
-                <div class="item"></div>
-                <div class="item"></div>
-                <div class="item"></div>
-
-                <div class="item"></div>
-                <div class="item"></div>
-                <div class="item"></div>
-
-                <div class="item"></div>
-                <div class="item"></div>
-                <div class="item"></div>
-
-                <div class="item"></div>
-                <div class="item"></div>
-                <div class="item"></div>
-
-                <div class="item"></div>
-                <div class="item"></div>
-                <div class="item"></div>
-
-                <div class="item"></div>
-                <div class="item"></div>
-                <div class="item"></div>
-
-                <div class="item"></div>
-                <div class="item"></div>
-                <div class="item"></div>
-
-                <div class="item"></div>
-                <div class="item"></div>
-                <div class="item"></div>
-        </div>`
-        div.appendChild(caja);
-
- }*/
+          
+        `
+        cajita.appendChild(gio)
+    });
+}
+ola();*/
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Obtén la referencia al contenedor donde se mostrarán las recetas
+var recetasContainer = document.getElementById("recetas");
+
+// Recorre las recetas y genera el HTML para cada una
+for (var i = 0; i < recetas.length; i++) {
+  var receta = recetas[i];
+
+  // Crea los elementos HTML para mostrar la receta
+  var recetaDiv = document.createElement("div");
+  var nombreHeader = document.createElement("h2");
+  var descripcionParagraph = document.createElement("p");
+  var imagen = document.createElement("img");
+  var ingredientesList = document.createElement("ul");
+  var preparacionList = document.createElement("ol");
+
+  // Asigna los valores de la receta a los elementos HTML
+  nombreHeader.textContent = receta.nombre;
+  descripcionParagraph.textContent = receta.descripcion;
+  imagen.src = receta.img;
+
+  // Agrega los ingredientes a la lista de ingredientes
+  for (var j = 1; j <= 8; j++) {
+  var ingredienteKey = "ingrediente" + j;
+    var ingrediente = receta.ingredientes[ingredienteKey];
+    if (ingrediente) {
+      var ingredienteItem = document.createElement("li");
+      ingredienteItem.textContent = ingrediente;
+      ingredientesList.appendChild(ingredienteItem);
+    }
+  }
+
+  // Agrega los pasos de preparación a la lista de preparación
+  for (var k = 1; k <= 4; k++) {
+    var pasoKey = "paso" + k;
+    var paso = receta.preparacion[pasoKey];
+    if (paso) {
+      var pasoItem = document.createElement("li");
+      pasoItem.textContent = paso;
+      preparacionList.appendChild(pasoItem);
+    }
+  }
+
+  // Agrega los elementos HTML de la receta al contenedor
+  recetaDiv.appendChild(nombreHeader);
+  recetaDiv.appendChild(descripcionParagraph);
+  recetaDiv.appendChild(imagen);
+  recetaDiv.appendChild(document.createElement("h3")).textContent = "Ingredientes:";
+  recetaDiv.appendChild(ingredientesList);
+  recetaDiv.appendChild(document.createElement("h3")).textContent = "Preparación:";
+  recetaDiv.appendChild(preparacionList);
+
+  recetasContainer.appendChild(recetaDiv);
+}*/
--- js/components/refaccion/sabias_que.js
+++ js/components/refaccion/sabias_que.js
@@ -1,11 +1,17 @@
-// Componente sabias que
 function sabias_q (){
-    let sabiass = document.querySelector(".caja");
+
+    let sabiass = document.querySelector(".box_sabias");
     let gatito = document.createElement("div")
 
     gatito.innerHTML= `
-        <div class= "ssabias_q>
+        <div class= "ssabias_q">
         <h1 class= "titulo_sabias">¿Sabias qué?</h1>
-        <p class= "cuerpo"></p>
+        <p class= "cuerpo">
+        rgjrjgreiufffvfjvfbvhjifiuf
+        </p>
+        </div>
+        
     `
+    sabiass.appendChild(gatito);
+    console.log(gatito)
 }
