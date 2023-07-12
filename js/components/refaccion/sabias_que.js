13aecb1d0a8249899c2b13ec27d83a3a0cc36abb
Claudia <clperez@scl.edu.gt>
2023-07-11 20:13:16 -0600

    Diseño

--- js/components/refaccion/sabias_que.js
+++ js/components/refaccion/sabias_que.js
@@ -1,1 +1,11 @@
 // Componente sabias que
+function sabias_q (){
+    let sabiass = document.querySelector(".caja");
+    let gatito = document.createElement("div")
+
+    gatito.innerHTML= `
+        <div class= "ssabias_q>
+        <h1 class= "titulo_sabias">¿Sabias qué?</h1>
+        <p class= "cuerpo"></p>
+    `
+}
