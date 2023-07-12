1f529cab32732c3ba4718c5aae9e58128d37b56f
Claudia <clperez@scl.edu.gt>
2023-07-12 08:29:47 -0600

    sabias que

--- js/components/refaccion/sabias_que.js
+++ js/components/refaccion/sabias_que.js
@@ -4,14 +4,19 @@ function sabias_q (){
     let gatito = document.createElement("div")
 
     gatito.innerHTML= `
-        <div class= "ssabias_q">
-        <h1 class= "titulo_sabias">¿Sabias qué?</h1>
-        <p class= "cuerpo">
-        rgjrjgreiufffvfjvfbvhjifiuf
-        </p>
-        </div>
+    <div class="box_sabias">
+    <h1 class= "titulo_sabias">¿Sabias qué?</h1>
+ <p class="cuerpo">
+    Tomar una refacción adecuada durante el día puede aumentar
+    la productividad y mejorar la concentración. Al proporcionar 
+    energía y nutrientes a nuestro cuerpo, podemos mantenernos alerta 
+    y enfocados en nuestras actividades.
+ </p>
+</div>
         
     `
     sabiass.appendChild(gatito);
     console.log(gatito)
 }
+
+
