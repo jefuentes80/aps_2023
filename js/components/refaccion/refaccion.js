b1f0ac46b96201613d79138f74b2a0f1b5eded32
Claudia <clperez@scl.edu.gt>
2023-07-13 07:47:04 -0600

    sabias que

--- js/components/refaccion/refaccion.js
+++ js/components/refaccion/refaccion.js
@@ -1,14 +1,29 @@
-import { recetas } from "./prueba.js";
+import { recetas } from "./data_refaccion.js";
+
+
+
+
+
+
 
 function ola(){
 
+    
     let cajita = document.querySelector(".recetas")
+
+    let texto = document.querySelector()
+    texto.innerHTML=
+    `
+    <h1 class="titulo">Recetas de cocina</h1>
+        <div class="recetas"></div>
+    `
     
     recetas.forEach(element => {
 
         let gio = document.createElement("div")
         gio.classList.add("item")
         gio.innerHTML= `
+        
         <img src= "${element["img"]}" class="imagenes_re">
             <p class="nombres">${element["nombre"]} </p>
             <p class="descripcion">${element["descripcion"]} </p>
