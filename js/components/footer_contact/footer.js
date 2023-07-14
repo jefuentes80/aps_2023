0e7cf0b49eb6e8a7b482789fefd8f76b93aebd75
Pablox <palchavez@scl.edu.gt>
2023-07-13 22:34:43 -0600

    13-07-2023.2

--- js/components/footer_contact/footer.js
+++ js/components/footer_contact/footer.js
@@ -40,18 +40,41 @@ function llamar_footer() {
         <div class="information">
             <div class="item1">
                 <h2>En qué consiste este proyecto?</h2>
-                <p>
-                    Este proyecto es un recetario de distintos platillos saludables 
-                </p>
+                <div class="element">
+                    <p>
+                        Este proyecto es un recetario de distintos platillos saludables y bastante
+                        fáciles de preparar ya que son platillos que no requieren de una gran preparación, además
+                        contamos con diferentes platillos para cada tiempo de comida en el día a día.
+                    </p>
+                </div>
             </div>
 
             <div class="item2">
                 <h2>¿Qué tecnologías se utilizaron en la creación de este proyecto?</h2>
-                <p>
-                   Este proyecto está enfocado dar a conocer platillos nutritivos y saludables
-                    para todos aquellos consumidores que desean comer alimentos más nutritivos en cada
-                    tiempo de comida y de manera rápida.
-                </p>
+                <div class="element">
+                    <p>
+                        Para este pryecto se trabajo con las tecnologías de HTML, CSS y Javascript.
+                    </p>
+                </div>
+            </div>
+
+            <div class="item3">
+                <h2>¿Cuál es el propósito de este proyecto?</h2>
+                <div class="element">
+                    <p>
+                        Este proyecto está enfocado dar a conocer platillos nutritivos y saludables
+                        para todos aquellos consumidores que desean comer alimentos más nutritivos en cada
+                        tiempo de comida y de manera rápida.
+                    </p>
+                </div>
+            </div>
+
+            <div class="item4">
+                <div class="logos">
+                    <a href="http://instagram.com/school_scl/"><img src="https://s.widget-club.com/samples/0GbysnQNNCfFuz9JwUk4i6kdJgx1/DRMH2OSVT9ojTa2y3T2P/25603244-47FD-4D1A-A1B4-97B72696ED7B.jpg?q=70"></a>
+                    <a href="https://wa.me/50232150777"><img src="https://s.widget-club.com/samples/tFag1IrqmkbYT4YcoN26yXx6h472/4OCvVtglu58tak2mn5LX/5AD9E895-7A78-48AB-9853-AD07C57F7EF4.jpg?q=70"></a>
+                    <a href="https://www.facebook.com/colegioSCL/"><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSWs5aZP5X-QJbxayc68LJwUn3xPxOFYH-PI1E6fu9BJbM5C-Ad"></a>
+                </div>
             </div>
         </div>
         
